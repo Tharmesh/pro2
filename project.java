885c8d76f799c1287a58f0366ddaf4fb1eb100cb
tharmesh <tharmeshtharmesh728@gmail.com>
2023-02-16 17:00:57 +0530

    3rd commit

--- project.java
+++ project.java
@@ -1,6 +1,6 @@
 class project {
     public static void main(String[] args) {
-        int n = 123, s = 0, d;
+        int n = 124, s = 0, d;
         for (; n != 0; n /= 10) {
             d = n % 10;
             s += d;
